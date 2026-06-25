90a92b1a3ce6e4fc38b890fbf0d30b529b2b1ac6
Mo'men <mmm.isam.99@gmail.com>
2026-06-25 11:44:18 +0300

    fix the deploauing issues 1

--- app/(pages)/booking/page.tsx
+++ app/(pages)/booking/page.tsx
@@ -1,17 +1,38 @@
 import { Booking } from "@/app/_modules/booking/entity/booking";
 import UserBookings from "@/app/_modules/booking/view/UserBookings";
 import { DYNAMIC_PAGE_API_URL } from "@/utils/constance";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Your Booking page",
   description: "the best booking app",
 };
+export const dynamic = "force-dynamic";
 const BookingPage = async () => {
   const cookieStore = await cookies();
+  const headersList = await headers();
+
+  console.log("HOST:", headersList.get("host"));
+  console.log("COOKIE HEADER:");
+  console.log(headersList.get("cookie"));
+
+  console.log("ACCESS TOKEN:");
+  console.log(cookieStore.get("access_token"));
+  console.log("HOST:", process.env.VERCEL_URL);
+  console.log(
+    cookieStore.getAll().map((cookie) => ({
+      name: cookie.name,
+      value: cookie.value.substring(0, 10),
+    })),
+  );
+  console.log(
+    "cookies:",
+    cookieStore.getAll().map((c) => c.name),
+  );
   const allCookies = cookieStore.toString();
-  const token = cookieStore.get("token")?.value;
+  const token = cookieStore.get("access_token")?.value;
+
   const response = await fetch(
     `${DYNAMIC_PAGE_API_URL}/api/booking/my-bookings`,
     {
@@ -22,6 +43,7 @@ const BookingPage = async () => {
     },
   );
   if (!response.ok) {
+    console.log("the response is :", await response.json());
     return <>some thing went wrong</>;
   }
   const bookings = (await response.json()) as Booking[];
